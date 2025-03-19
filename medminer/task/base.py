9d2287505021d4d05d5eb083a32741c6f5de9596
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-03-19 21:15:37 +0000

    feat: add base task

---
+++ medminer/task/base.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from enum import StrEnum, auto
+from typing import Any, Type
+
+from smolagents import CodeAgent, Model, MultiStepAgent, Tool, ToolCallingAgent
+
+
+class Agent(StrEnum):
+    CODEAGENT = auto()
+    TOOLCALLINGAGENT = auto()
+    MULTISTEPAGENT = auto()
+
+
+@dataclass
+class Task:
+    prompt: str
+    tools: list[Tool] = []
+    agent_type: Agent = Agent.TOOLCALLINGAGENT
+    agent_params: dict[str, Any] = {}
+
+    @property
+    def agent(self) -> Type[MultiStepAgent]:
+        match self.agent_type:
+            case Agent.CODEAGENT:
+                return CodeAgent
+            case Agent.TOOLCALLINGAGENT:
+                return ToolCallingAgent
+            case Agent.MULTISTEPAGENT:
+                return MultiStepAgent
+            case _:
+                return ToolCallingAgent
+
+    def run(self, model: Model, **kwargs: Any) -> Any:
+        kwargs = self.agent_params | kwargs
+        agent = self.agent(self.tools, model, **kwargs)
+        
+        return agent.run(self.prompt)
+
+        

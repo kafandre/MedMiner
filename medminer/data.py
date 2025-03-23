87b73d7e25a1133d7f73708b5d4b7f293e3bcd9c
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-03-23 19:20:07 +0000

    feat: add dataclass for docs

---
+++ medminer/data.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from textwrap import dedent
+
+
+@dataclass
+class Document:
+    patient_id: str
+    text: str
+
+    @property
+    def content(self) -> str:
+        return dedent(
+            f"""
+            Patiend: {self.patient_id}
+
+            {self.content}
+        """
+        )

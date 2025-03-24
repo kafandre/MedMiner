9d80b47a40401ee1b3d928b0ce8f2f2c8657f3d3
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-03-24 18:44:11 +0000

    wip: clean up

--- medminer/task/medication/__init__.py
+++
@@ -1,3 +0,0 @@
-from medminer.task.medication.task import medication_task
-
-__all__ = ["medication_task"]
--- medminer/task/medication/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from medminer.task.medication.tools.csv import save_csv
-
-__all__ = ["save_csv"]

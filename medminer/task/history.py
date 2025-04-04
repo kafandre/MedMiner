d23f689926463311364f8c7bff9e0c49076801e0
Christian Porschen <christian.porschen@gmail.com>
2025-04-04 14:30:56 +0200

    wip: api exploration

--- medminer/task/history.py
+++ medminer/task/history.py
@@ -1,17 +1,32 @@
 from textwrap import dedent
 
 from medminer.task import Task
-from medminer.tools import save_csv
+from medminer.tools import extract_diagnosis_data, get_diagnosis_info, save_csv
 
 history_task = Task(
     name="history",
     prompt=dedent(
         """\
-        Given a medical history of a patient, extract all given diagnoses and save all information as csv. The diagnosis should be translated to english. The medical history is usually in the format of a sentence or a paragraph. These are the steps you should follow to complete the task:
+        Given a medical history of a patient, extract all given diagnoses and save all information as csv. The diagnosis should be translated to english. The medical history is usually in the format of a sentence or a paragraph. Every diagnosis should have a single row, if there are multiple diagnosis that can be extracted from a single piece of text, split them up. These are the steps you should follow to complete the task:
         1. extract a part of the text that contains a diagnosis. The diagnosis can be in any language. This is the diagnosis_reference column.
         2. translate the diagnoses to english if necessary and infer the diagnosis_translated column.
         3. Extract the relevant diagnosis as a string and loose everything that is not relevant. This is the diagnosis column.
         4. Extract the month and year of the medical history for that diagnosis. If not applicable, write an empty string.
+
+        Example 1:
+        Input: "schwere Herzinsuffizienz, 2. Grad, NYHA II, ED 07/2023"
+        Output: [{"patient_id": 1, "diagnosis_reference": "schwere chronische Herzinsuffizienz, 2. Grad, NYHA II", "diagnosis_translated": "severe chronic heart failure, 2nd degree, NYHA II", "diagnosis": "chronic heart failure", "month": "7", "year": "2023"}]
+
+        Example 2:
+        Input: "Myocardial Infarction"
+        Output: [{"patient_id": 2, "diagnosis_reference": "Myocardial Infarction", "diagnosis_translated": "Myocardial Infarction", "diagnosis": "Myocardial Infarction", "month": "", "year": ""}]        
+
+        Example 3:
+        Input: "Lungenteilresektion rechts bei Lungenmetastasen eines Kolonkarzinoms, 2023-07-01"
+        Output: [
+            {"patient_id": 3, "diagnosis_reference": "Lungenteilresektion rechts bei Lungenmetastasen eines Kolonkarzinoms", "diagnosis_translated": "Lung resection due to lung metastases of colon cancer", "diagnosis": "colon cancer", "month": "7", "year": "2023"},
+            {"patient_id": 3, "diagnosis_reference": "Lungenteilresektion rechts bei Lungenmetastasen eines Kolonkarzinoms", "diagnosis_translated": "Lung resection due to lung metastases of colon cancer", "diagnosis": "lung metastasis", "month": "7", "year": "2023"}
+        ]
 
         save the the following columns:
         - patient_id: The patient ID.
@@ -22,5 +37,5 @@ history_task = Task(
         - year: The year of the medical history. if not applicable, write an empty string.
         """
     ),
-    tools=[save_csv],
+    tools=[save_csv, extract_diagnosis_data, get_diagnosis_info],
 )
--- medminer/tools/diagnosis.py
+++ medminer/tools/diagnosis.py
@@ -5,32 +5,35 @@ from smolagents import tool
 @tool
 def extract_diagnosis_data(
     data: list[dict],
-    
 ) -> list[dict]:
     """
-    Extract diagnosis data from the provided list of dictionaries.
-
+    Adds extracted data to the task memory.
+    
     Args:
-        data (list[dict]): A list of dictionaries containing medical data to save.
-        All dictionaries must have the following keys:
-        - patient_id: str, the ID of the patient.
-        - diagnosis: str, the diagnosis of the patient.
-        - diagnosis_corrected: str, the corrected and translated diagnosis of the patient.
-        - year: int, the year of the diagnosis.
-        - month: int, the month of the diagnosis.
+        data: A list of dictionaries containing the data to save
+        
+            All dictionaries must have the following keys.
+            - patient_id: The patient ID.
+            - diagnosis_reference: The diagnosis of the medical history found in the text.
+            - diagnosis_translated: The corrected diagnosis of the medical history, translated to english.
+            - diagnosis: The extracted diagnosis.
+            - month: The month of the medical history. if not applicable, write an empty string.
+            - year: The year of the medical history. if not applicable, write an empty string.
+    
     Returns:
-         A message indicating where the data was saved.
+        A message indicating where the data was saved.
+        
     Example:
         >>> data = [
-        ...     {"patient_id": 1, "diagnosis": "Heart Attack", "diagnosis_corrected": "Myocardial Infarction", "year": 2020, "month": 5},
-        ...     {"patient_id": 2, "medication_name": "Paracetamol"},
+        ...     {"patient_id": 1, "diagnosis": "Myocardial Infarction"},
+        ...     {"patient_id": 2, "diagnosis": "colon cancer"},
         ... ]
         >>> extract_diagnosis_data("diagnosis", data)
     """
-    return data
 
 
 @tool
 def get_diagnosis_info(
-    diagnosis_names: str,
-)
+    diagnosis_names: list[str],
+) -> dict:
+    pass

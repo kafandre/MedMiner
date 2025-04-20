a6f693f2b18c51864e7586b44f59e4385fc2f33d
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-04-20 22:24:31 +0000

    wip: rework input ui

--- medminer/ui/app.py
+++ medminer/ui/app.py
@@ -2,6 +2,21 @@ import gradio as gr
 
 from medminer.ui.api import TaskType, process_files, process_sql, process_text
 
+try:
+    from smolagents import TransformersModel
+
+    impoted_hf_transformer = True
+except ImportError:
+    impoted_hf_transformer = False
+
+try:
+    from smolagents import AzureOpenAIServerModel
+
+    imported_azure_openai = True
+except ImportError:
+    imported_azure_openai = False
+
+
 with gr.Blocks(
     title="MedMiner",
 ) as demo:
@@ -13,34 +28,59 @@ with gr.Blocks(
     )
     with gr.Row():
         with gr.Column(scale=1):
-            input_text = gr.Markdown("## Input")
+            gr.Markdown("## Model")
+            with gr.Tabs() as model_tabs:
+                if impoted_hf_transformer:
+                    with gr.Tab("HF Transformer", id="hf_transformer"):
+                        gr.Markdown("Enter a Model name of a HuggingFace transformer.")
+                        hf_model = gr.Textbox(label="Model name", placeholder="Qwen/Qwen2.5-Coder-32B-Instruct")
+
+                if imported_azure_openai:
+                    with gr.Tab("Azure OpenAI", id="azure_openai"):
+                        gr.Markdown("Enter the model name, endpoint, and API key.")
+                        hf_model = gr.Textbox(label="Model name", placeholder="")
+                        hf_endpoint = gr.Textbox(label="Endpoint", placeholder="")
+                        hf_version = gr.Textbox(label="API Version", placeholder="")
+                        hf_key = gr.Textbox(label="API Key", placeholder="", type="password")
+
+            gr.Markdown("## Task")
             tasks_input = gr.CheckboxGroup(
-                label="Task",
-                info="Select the task you want to perform.",
+                label="Select the task you want to perform.",
                 choices=TaskType.list(),
             )
 
-            with gr.Tab("Files"):
-                files_tab_text = gr.Markdown("Upload a file to extract information from it.")
-                files_input = gr.Files(label="Upload a files", file_types=[".txt", ".csv"], file_count="multiple")
-                process_files_btn = gr.Button("Process Files")
+            gr.Markdown("## Data")
+            with gr.Tabs() as data_tabs:
+                with gr.Tab(".txt Files"):
+                    gr.Markdown("Upload a file to extract information from it.")
+                    txt_files_input = gr.Files(label="Upload a files", file_types=[".txt"], file_count="multiple")
+                    process_txt_files_btn = gr.Button("Process Files")
 
-            with gr.Tab("SQL"):
-                sql_tab_text = gr.Markdown("Enter a SQL query to extract information from a database.")
-                sql_input = gr.Textbox(label="SQL Query", placeholder="SELECT patient_id, text FROM patient_notes")
-                process_sql_btn = gr.Button("Process SQL")
+                with gr.Tab("CSV"):
+                    gr.Markdown("Upload a file to extract information from it.")
+                    csv_file_input = gr.Files(
+                        label="Upload a files",
+                        file_types=[".csv"],
+                    )
+                    process_csv_file_btn = gr.Button("Process Files")
 
-            with gr.Tab("Text"):
-                text_tab_text = gr.Markdown("Enter a text to extract information from it.")
-                text_input = gr.Textbox(label="Text", placeholder="Enter text here", lines=25)
-                process_text_btn = gr.Button("Process Text")
+                with gr.Tab("SQL"):
+                    gr.Markdown("Enter a SQL query to extract information from a database.")
+                    sql_input = gr.Textbox(label="SQL Query", placeholder="SELECT patient_id, text FROM patient_notes")
+                    process_sql_btn = gr.Button("Process SQL")
+
+                with gr.Tab("Text"):
+                    gr.Markdown("Enter a text to extract information from it.")
+                    text_input = gr.Textbox(label="Text", placeholder="Enter text here", lines=25)
+                    process_text_btn = gr.Button("Process Text")
 
         with gr.Column(scale=1):
             output_text = gr.Markdown("## Output")
 
             output_data = gr.Dataframe()
 
-        process_files_btn.click(process_files, inputs=[files_input, tasks_input], outputs=[output_data])
+        process_txt_files_btn.click(process_files, inputs=[txt_files_input, tasks_input], outputs=[output_data])
+        process_csv_file_btn.click(process_files, inputs=[csv_file_input, tasks_input], outputs=[output_data])
         process_sql_btn.click(process_sql, inputs=[sql_input, tasks_input], outputs=[output_data])
         process_text_btn.click(process_text, inputs=[text_input, tasks_input], outputs=[output_data])
 

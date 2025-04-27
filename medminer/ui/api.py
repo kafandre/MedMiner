7f7c4b27cb099c73e6ebcecb72c1957188f79f98
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-04-27 18:21:54 +0000

    feat: add pipeline

--- medminer/ui/api.py
+++ medminer/ui/api.py
@@ -4,23 +4,15 @@ import pandas as pd
 def process_txt_files(
     files: list | None, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str]
 ) -> dict[str, pd.DataFrame]:
-    """
-    Process a list of files with the specified tasks.
+    """Process a list of files with the specified tasks.
 
-    Parameters
-    ----------
-    files : list
-        List of file paths to process.
-    model_settings : dict[str, str]
-        Model settings for the processing.
-    task_settings : dict[str, str]
-        Task settings for the processing.
-    tasks : list[str]
-        List of tasks to perform on the files.
+    Args:
+        files: List of file paths to process.
+        model_settings: Model settings for the processing.
+        task_settings: Task settings for the processing.
+        tasks: List of tasks to perform on the files.
 
-    Returns
-    -------
-    dict[str, pd.DataFrame]
+    Returns:
         Dictionary containing the processed data.
     """
     if files is None or not tasks:
@@ -36,25 +28,16 @@ def process_csv_file(
     task_settings: dict[str, str],
     tasks: list[str],
 ) -> dict[str, pd.DataFrame]:
-    """
-    Process a CSV file with the specified tasks.
+    """Process a CSV file with the specified tasks.
 
-    Parameters
-    ----------
-    file : str | None
-        Path to the CSV file to process.
-    column : str | None
-        Name of the column to process.
-    model_settings : dict[str, str]
-        Model settings for the processing.
-    task_settings : dict[str, str]
-        Task settings for the processing.
-    tasks : list[str]
-        List of tasks to perform on the files.
+    Args:
+        file: Path to the CSV file to process.
+        column: Name of the column to process.
+        model_settings: Model settings for the processing.
+        task_settings: Task settings for the processing.
+        tasks: List of tasks to perform on the files.
 
-    Returns
-    -------
-    dict[str, pd.DataFrame]
+    Returns:
         Dictionary containing the processed data.
     """
     if file is None or not column or not tasks:
@@ -66,23 +49,15 @@ def process_csv_file(
 def process_sql(
     sql: str, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str]
 ) -> dict[str, pd.DataFrame]:
-    """
-    Process a SQL query with the specified tasks.
+    """Process a SQL query with the specified tasks.
 
-    Parameters
-    ----------
-    sql : str
-        SQL query to process.
-    model_settings : dict[str, str]
-        Model settings for the processing.
-    task_settings : dict[str, str]
-        Task settings for the processing.
-    tasks : list[str]
-        List of tasks to perform on the files.
+    Args:
+        sql: SQL query to process.
+        model_settings: Model settings for the processing.
+        task_settings: Task settings for the processing.
+        tasks: List of tasks to perform on the files.
 
-    Returns
-    -------
-    dict[str, pd.DataFrame]
+    Returns:
         Dictionary containing the processed data.
     """
     if not sql or not tasks:
@@ -94,23 +69,15 @@ def process_sql(
 def process_text(
     text: str, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str]
 ) -> dict[str, pd.DataFrame]:
-    """
-    Process a text with the specified tasks.
+    """Process a text with the specified tasks.
 
-    Parameters
-    ----------
-    text : str
-        Text to process.
-    model_settings : dict[str, str]
-        Model settings for the processing.
-    task_settings : dict[str, str]
-        Task settings for the processing.
-    tasks : list[str]
-        List of tasks to perform on the files.
+    Args:
+        text: Text to process.
+        model_settings: Model settings for the processing.
+        task_settings: Task settings for the processing.
+        tasks: List of tasks to perform on the files.
 
-    Returns
-    -------
-    dict[str, pd.DataFrame]
+    Returns:
         Dictionary containing the processed data.
     """
     if not text or not tasks:

924474b9558042bc545bf863285d8fea549d00b2
Paul-B98 <115164840+Paul-B98@users.noreply.github.com>
2025-04-28 18:06:38 +0000

    feat: add api for txt files and csv

--- medminer/ui/api.py
+++ medminer/ui/api.py
@@ -1,13 +1,59 @@
+from enum import Enum
+from typing import Type
+
 import gradio as gr
 import pandas as pd
 
-from medminer.pipe import SingleAgentPipeline
+from medminer.pipe import MultiAgentPipeline, Pipeline, SingleAgentPipeline
 from medminer.task.base import TaskRegistry
 from medminer.utils.models import DefaultModel
 
 
+class AgentMode(Enum):
+    SINGLE = 0
+    MULTI = 1
+
+
+def _process(
+    data: list[str], model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str], agent: str
+) -> dict[str, pd.DataFrame]:
+    """Process the data with the specified tasks.
+
+    Args:
+        data: List of data to process.
+        model_settings: Model settings for the processing.
+        task_settings: Task settings for the processing.
+        tasks: List of tasks to perform on the files.
+        agent: Agent mode (single or multi).
+
+    Returns:
+        Dictionary containing the processed data.
+    """
+    if not data or not tasks:
+        return {}
+
+    reg = TaskRegistry()
+
+    pipe_csl: Type[Pipeline] = SingleAgentPipeline if agent == AgentMode.SINGLE else MultiAgentPipeline
+
+    pipe = pipe_csl(
+        tasks=reg.filter(tasks),
+        model=DefaultModel(**model_settings).model,
+        session_id=None,
+        **task_settings,
+    )
+
+    dfs = pipe.run(data)
+
+    return {task_name.capitalize(): df for task_name, df in dfs.items()}
+
+
 def process_txt_files(
-    files: list | None, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str]
+    files: list | None,
+    model_settings: dict[str, str],
+    task_settings: dict[str, str],
+    tasks: list[str],
+    agent: str,
 ) -> dict[str, pd.DataFrame]:
     """Process a list of files with the specified tasks.
 
@@ -23,7 +69,18 @@ def process_txt_files(
     if files is None or not tasks:
         return {}
 
-    return {}
+    data: list[str] = []
+    for file in files:
+        with open(file, "r") as f:
+            data.append(f.read())
+
+    return _process(
+        data=data,
+        model_settings=model_settings,
+        task_settings=task_settings,
+        tasks=tasks,
+        agent=agent,
+    )
 
 
 def process_csv_file(
@@ -32,6 +89,7 @@ def process_csv_file(
     model_settings: dict[str, str],
     task_settings: dict[str, str],
     tasks: list[str],
+    agent: str,
 ) -> dict[str, pd.DataFrame]:
     """Process a CSV file with the specified tasks.
 
@@ -48,11 +106,20 @@ def process_csv_file(
     if file is None or not column or not tasks:
         return {}
 
-    return {}
+    df = pd.read_csv(file)
+    data: list[str] = df[column].tolist()
+
+    return _process(
+        data=data,
+        model_settings=model_settings,
+        task_settings=task_settings,
+        tasks=tasks,
+        agent=agent,
+    )
 
 
 def process_sql(
-    sql: str, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str]
+    sql: str, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str], agent: str
 ) -> dict[str, pd.DataFrame]:
     """Process a SQL query with the specified tasks.
 
@@ -72,7 +139,12 @@ def process_sql(
 
 
 def process_text(
-    request: gr.Request, text: str, model_settings: dict[str, str], task_settings: dict[str, str], tasks: list[str]
+    request: gr.Request,
+    text: str,
+    model_settings: dict[str, str],
+    task_settings: dict[str, str],
+    tasks: list[str],
+    agent: str,
 ) -> dict[str, pd.DataFrame]:
     """Process a text with the specified tasks.
 
@@ -88,15 +160,10 @@ def process_text(
     if not text or not tasks:
         return {}
 
-    reg = TaskRegistry()
-
-    pipe = SingleAgentPipeline(
-        tasks=reg.filter(tasks),
-        model=DefaultModel(**model_settings).model,
-        session_id=request.session_hash,
-        **task_settings,
+    return _process(
+        data=[text],
+        model_settings=model_settings,
+        task_settings=task_settings,
+        tasks=tasks,
+        agent=agent,
     )
-
-    dfs = pipe.run([text])
-
-    return {task_name.capitalize(): df for task_name, df in dfs.items()}
